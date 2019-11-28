e737ac41ae5dda0fecb0e651511cbe1748540d77
GonzaPM7 <gonzap03@ucm.es>
2019-11-28 15:35:16 +0100

    Añadido salto y movimiento

---
+++ Assets/Scripts/MovimientoProtagonista.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoProtagonista : MonoBehaviour
+{
+
+    public float speed;
+    float moved = 0f;
+
+    Rigidbody2D rb;
+
+    public float climbSpeed;
+    
+
+    bool control = true;
+
+    bool canClimb = false;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        control = true;
+    }
+    void Update()
+    {  
+         
+        if (control)
+        {
+            Movimiento();
+        }
+
+        if (canClimb)
+            Ladder();
+    }
+
+    public void Movimiento()
+    {
+        moved = Input.GetAxis("Horizontal");
+        if (moved < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;            
+        }
+        else if (moved > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;            
+        }
+        
+        rb.velocity = new Vector2(moved * speed, rb.velocity.y);
+    }
+
+    void Ladder()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, 1) * climbSpeed;
+            rb.gravityScale = 0;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0, -1) * climbSpeed;
+            rb.gravityScale = 0;
+        }
+        rb.gravityScale = 1;
+    }    
+
+    public void CanClimb(bool can)
+    {
+        canClimb = can;
+    }
+
+}
---
+++ Assets/Scripts/SaltoHumano.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltoHumano : MonoBehaviour
+{
+
+    int NSaltos = 2;
+    public int Fuerza;
+    public GameObject jugador;
+   // public Rigidbody2D rb;    
+    
+
+    void Start()
+    {
+       // rb = GetComponent<Rigidbody2D>();       
+    }
+    /*public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Suelo1")
+        {
+            salto = true;
+        }
+    }*/
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("x");
+        if (collision.gameObject.tag == "Suelo" || collision.gameObject.tag == "Suelo1" || collision.gameObject.tag =="Caja")
+        {
+            NSaltos = 2;
+        }
+    }
+
+    public void Update()
+    {
+       /* if (rb.velocity.y == 0)
+        {
+            NSaltos = 2;
+        }*/
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (NSaltos > 0)
+            {
+                jugador.GetComponent<Rigidbody2D>().velocity= new Vector2(0, Fuerza);
+                NSaltos -= 1;
+            }
+        }        
+
+    }
+
+}
