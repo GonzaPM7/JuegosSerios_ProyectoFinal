74cb2de057d75ad68e2c56100845f36e31c25318
GonzaPM7 <gonzap03@ucm.es>
2019-12-05 12:32:55 +0100

    Creado el menu principal y empezando con el  nivel 1

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Nivel 1");
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
