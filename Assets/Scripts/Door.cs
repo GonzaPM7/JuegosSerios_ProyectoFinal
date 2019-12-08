e3e478b77d581732e62c52615ada964f5aaef666
GonzaPM7 <gonzap03@ucm.es>
2019-12-08 14:18:55 +0100

    Puerta, muerte instantánea

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    public string scene;
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
---
+++ Assets/Scripts/InstantDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstantDeath : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            string scene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
